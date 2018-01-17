168fb53554f163d03a767b5fb9a120d7a5ba2b2a
Taiyyab Ahmed <hafiztaiyyab11@gmail.com>
2018-01-16 20:43:44 -0600

    Created Array Directory

---
+++ Array/MyArray.cc
@@ -0,0 +1,18 @@
+class MyArray
+{
+    int size;
+    int capacity;
+    public:
+        int size();
+        int capacity();
+        bool is_empty();
+        int at(index);
+        void push(item);
+        void insert(index, item);
+        int pop();
+        void deletey(index);
+        void remove(item);
+        void find(item);
+        this resize(new_capacity);
+    
+    }
